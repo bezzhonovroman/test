f433186e49a2aa6600432f515b3c255e4298824a
Roman Bezzhonov <bezzhonov@inbox.ru>
2022-09-28 14:05:50 +0300

    papapanin

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+    // Owl Carousel
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+        center: true,
+        items: 1,
+        dots: true,
+        nav: true,
+    });
+})
